738e4f432b4766f53f70808f8bae0e86ae1a58ed
Doreapp <amandin@kth.se>
2021-11-29 10:50:04 +0100

    strech map image in the full screen

--- components/Maps.js
+++ components/Maps.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
+import { Dimensions } from "react-native";
+
+
 
 
 export default class Maps extends Component {
     render() {
+        
+        const s_width = Dimensions.get('window').width; //full width
+        const s_height = Dimensions.get('window').height; //full height
+
         console.log("Render map")
         return (
             <Image
                 // style={styles.stretch}
                 style={{
-                    width:160,
-                    height:80
+                    width:s_width,
+                    height:s_height
                 }}
                 source={require('../img/maps/01.png')}
             />
